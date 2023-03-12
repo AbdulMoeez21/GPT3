93658194def401de949c0a760fa17883b2bf0f1a
AbdulMoeez21 <moizrao7@gmail.com>
2023-03-12 21:10:28 +0500

    responsive

--- src/components/SectionCards.jsx
+++ src/components/SectionCards.jsx
@@ -14,7 +14,7 @@ const SectionCards = () => {
             <span className='w-10 h-1  -mt-2 bg-gradient-to-r from-[#AE67FA]  via-[#F49867]  to-[#F49867]'></span>
         <div className="main flex -ml-5">
            <h1 className='text-white md:w-40 w-40 font-semibold '>{item.heading} </h1>
- <small className=' md:pl-32 pl-10 md:w-2/3 w-2/4  '>{item.paragraph}</small>
+ <small className=' md:pl-32 pl-10 md:w-2/3 w-4/6  '>{item.paragraph}</small>
  </div>
            </div></div>)
          })
